064c58ab9b52b77a5e4e50b6626cc0aba5243129
Hyunjun KIM <hihj070914@icloud.com>
2024-01-06 23:53:42 +0900

    feat: add day0/ex00

---
+++ piscine0/ex00/hello.py
@@ -0,0 +1,9 @@
+ft_list = ["Hello", "World!"]
+ft_tuple = ("Hello", "Korea!")
+ft_set = {"Hello", "Seoul!"}
+ft_dict = {"Hello": "42Seoul!"}
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
