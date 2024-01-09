618c56ee1b78ecc65044512fdaa89866303bbe4e
Hyunjun KIM <hihj070914@icloud.com>
2024-01-09 12:25:33 +0900

    feat: ex02

--- piscine0/ex02/find_ft_type.py
+++ piscine0/ex02/find_ft_type.py
@@ -1,2 +1,8 @@
 def all_thing_is_obj(object: any) -> int:
-    print(type(object))
+    if type(object) is None or isinstance(object, int):
+        print('Type not found')
+    elif isinstance(object, str):
+        print(f'{object} is in the kitchen : ', type(object))
+    else:
+        print(type(object))
+    return 42
