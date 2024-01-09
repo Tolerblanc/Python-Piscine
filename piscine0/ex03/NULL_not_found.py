d23abddb8ac25df0a8ef8fdfd1712cfe292915b7
Hyunjun KIM <hihj070914@icloud.com>
2024-01-09 12:25:38 +0900

    feat: ex03

---
+++ piscine0/ex03/NULL_not_found.py
@@ -0,0 +1,15 @@
+def NULL_not_found(object: any) -> int:
+    if object is None:
+        pass
+    elif isinstance(object, float):
+        pass
+    elif isinstance(object, int):
+        pass
+    elif isinstance(object, str):
+        pass
+    elif isinstance(object, bool):
+        pass
+    elif isinstance(object, complex):
+        pass
+    elif isinstance(object, float):
+        pass
