134006f367b9a509771b4f2d5554d8b1c1bf01e0
Hyunjun KIM <hihj070914@icloud.com>
2024-01-09 12:25:16 +0900

    feat: ex00

--- piscine0/ex00/hello.py
+++ piscine0/ex00/hello.py
@@ -1,7 +1,13 @@
-ft_list = ["Hello", "World!"]
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
+
+ft_list[1] = "World!"
 ft_tuple = ("Hello", "Korea!")
-ft_set = {"Hello", "Seoul!"}
-ft_dict = {"Hello": "42Seoul!"}
+ft_set.remove("tutu!")
+ft_set.add("Seoul!")
+ft_dict["Hello"] = "42Seoul!"
 
 print(ft_list)
 print(ft_tuple)
