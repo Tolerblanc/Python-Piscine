0b36f58a5bc46a67eafecf00d7bcbdc8e0b1eadc
Hyunjun KIM <hihj070914@icloud.com>
2024-01-09 23:21:55 +0900

    feat: ex03 NULL print

--- piscine0/ex03/NULL_not_found.py
+++ piscine0/ex03/NULL_not_found.py
@@ -1,15 +1,15 @@
 def NULL_not_found(object: any) -> int:
     if object is None:
-        pass
-    elif isinstance(object, float):
-        pass
-    elif isinstance(object, int):
-        pass
-    elif isinstance(object, str):
-        pass
-    elif isinstance(object, bool):
-        pass
-    elif isinstance(object, complex):
-        pass
-    elif isinstance(object, float):
-        pass
+        print(f'Nothing: {object} {type(object)}')
+    elif isinstance(object, float) and object != object:
+        print(f'Garlic: {object} {type(object)}')
+    elif isinstance(object, int) and object == 0:
+        print(f'Zero: {object} {type(object)}')
+    elif isinstance(object, str) and object == '':
+        print(f'Empty: {object} {type(object)}')
+    elif isinstance(object, bool) and object is False:
+        print(f'Fake: {object} {type(object)}')
+    else:
+        print('Type not found')
+        return 1
+    return 0
