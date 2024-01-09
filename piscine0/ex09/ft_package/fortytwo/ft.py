6523316bd84e59000a1f10bbc2afc03ad6436de3
Hyunjun KIM <hihj070914@icloud.com>
2024-01-09 23:23:04 +0900

    feat: ex09 - initial package

---
+++ piscine0/ex09/ft_package/fortytwo/ft.py
@@ -0,0 +1,14 @@
+def ft():
+    """
+    always return 42
+
+    Args:
+        None
+
+    Returns:
+        42
+
+    Exception:
+        Never Raised.
+    """
+    return 42
---
+++ piscine0/ex09/tester.py
@@ -0,0 +1,3 @@
+from ft_package.fortytwo.ft import ft
+
+print(ft())
