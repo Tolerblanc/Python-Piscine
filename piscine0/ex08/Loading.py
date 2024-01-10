e893829d1902531ef3d9d83e261059017f27f708
Hyunjun KIM <hihj070914@icloud.com>
2024-01-10 17:52:00 +0900

    fix: delete banned module - 사용 금지된 time 모듈 삭제

--- piscine0/ex08/Loading.py
+++ piscine0/ex08/Loading.py
@@ -1,5 +1,4 @@
 import sys
-import time
 
 
 def ft_tqdm(lst: range) -> None:
@@ -18,18 +17,12 @@ def ft_tqdm(lst: range) -> None:
     TODO: Iteration speed and Expected time
     """
     total = len(lst)
-    start_time = time.time()
 
     def print_progress_bar(iteration):
         percent = f'{int(100 * (iteration / float(total)))}'
         filled_length = int(50 * iteration // total)
         bar = '█' * filled_length + ' ' * (50 - filled_length)
-        current_time = time.time() - start_time
-        current_minute = int(current_time // 60)
-        current_second = int(current_time % 60)
-        sys.stdout.write(
-            f'\r{percent}%|{bar}| {iteration}/{total} '
-            f'[{current_minute:02}:{current_second:02}]')
+        sys.stdout.write(f'\r{percent}%|{bar}| {iteration}/{total}')
         sys.stdout.flush()
 
     for i, item in enumerate(lst):
