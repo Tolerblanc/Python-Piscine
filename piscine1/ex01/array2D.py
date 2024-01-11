3ec189cce25c9027c2347e97e60e0a63ebe6f297
Hyunjun KIM <hihj070914@icloud.com>
2024-01-11 16:33:30 +0900

    feat: day01 ex01 - docstring 추가 - 리스트 인덱스에 대한 예외 처리

--- piscine1/ex01/array2D.py
+++ piscine1/ex01/array2D.py
@@ -2,8 +2,30 @@ import numpy as np
 
 
 def slice_me(family: list, start: int, end: int) -> list:
-    np_family = np.array(family)
-    sliced_family = np_family[start:end]
-    print(f'My shape is : {np_family.shape}')
-    print(f'My new shape is : {sliced_family.shape}')
-    return sliced_family.tolist()
+    """
+    slice the list from start to end, and return the result
+
+    Args:
+        :param family (list): list of names
+        :param start (int): start index
+        :param end (int): end index
+
+    Returns:
+        list: sliced list
+
+    Exceptions:
+        ValueError: if start or end is not an integer
+        IndexError: if start or end is out of range
+    """
+    try:
+        np_family = np.array(family)
+        sliced_family = np_family[start:end]
+        print(f'My shape is : {np_family.shape}')
+        print(f'My new shape is : {sliced_family.shape}')
+        return sliced_family.tolist()
+    except ValueError:
+        print('ValueError: start or end is not an integer')
+    except IndexError:
+        print('IndexError: start or end is out of range')
+    except Exception as e:
+        print(e)
