915b934311a5500d106e0cf1d368747f9b5f4266
Hyunjun KIM <hihj070914@icloud.com>
2024-01-11 17:21:26 +0900

    feat: day01 ex05 - docstring 추가 - ft_grey 구현 변경

--- piscine1/ex05/pimp_image.py
+++ piscine1/ex05/pimp_image.py
@@ -2,42 +2,89 @@ import numpy as np
 
 
 def ft_invert(array: np.array) -> np.array:
+    """
+    invert the color of the image
+
+    Args:
+        array (np.array): image array
+
+    Returns:
+        np.array: inverted image array
+    """
     array = np.array(array)
     return 255 - array
 
 
 def ft_red(array: np.array) -> np.array:
+    """
+    keep only the red color of the image
+    zero out the green and blue channel
+
+    Args:
+        array (np.array): image array
+
+    Returns:
+        np.array: red-filtered image array
+    """
     array = np.array(array)
     red_array = array.copy()
-    red_array[:, :, 1] = 0  # Zero out green
-    red_array[:, :, 2] = 0  # Zero out blue
+    red_array[:, :, 1] = 0
+    red_array[:, :, 2] = 0
     return red_array
 
 
 def ft_green(array: np.array) -> np.array:
+    """
+    keep only the green color of the image
+    zero out the red and blue channel
+
+    Args:
+        array (np.array): image array
+
+    Returns:
+        np.array: green-filtered image array
+    """
     array = np.array(array)
     green_array = array.copy()
-    green_array[:, :, 0] = 0  # Zero out red
-    green_array[:, :, 2] = 0  # Zero out blue
+    green_array[:, :, 0] = 0
+    green_array[:, :, 2] = 0
     return green_array
 
 
 def ft_blue(array: np.array) -> np.array:
+    """
+    keep only the blue color of the image
+    zero out the red and green channel
+
+    Args:
+        array (np.array): image array
+
+    Returns:
+        np.array: blue-filtered image array
+    """
     array = np.array(array)
     blue_array = array.copy()
-    blue_array[:, :, 0] = 0  # Zero out red
-    blue_array[:, :, 1] = 0  # Zero out green
+    blue_array[:, :, 0] = 0
+    blue_array[:, :, 1] = 0
     return blue_array
 
 
 def ft_grey(array: np.array) -> np.array:
-    # Assuming pre-computed weights for red, green, blue
-    array = np.array(array)
-    weight_red = 0.299
-    weight_green = 0.587
-    weight_blue = 0.114
+    """
+    convert the image to grey scale
+    map each pixel to green channel value
 
-    grey_array = np.dot(
-        array[..., :3], [weight_red, weight_green, weight_blue])
-    grey_array = grey_array.astype(np.uint8)
-    return np.stack((grey_array,)*3, axis=-1)
+    Args:
+        array (np.array): image array
+
+    Returns:
+        np.array: grey-scale image array
+    """
+    grey_array = array.copy()
+    single_channel = grey_array[:, :, 1]
+
+    grey_array[:, :, 0] = single_channel
+    grey_array[:, :, 1] = single_channel
+    grey_array[:, :, 2] = single_channel
+
+    return grey_array
