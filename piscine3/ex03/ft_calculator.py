5a42cbdbf2d68ecd694a23b5083019968343257f
Hyunjun KIM <hihj070914@icloud.com>
2024-01-11 23:03:08 +0900

    feat: skeleton codes for day03 ex03, 04

---
+++ piscine3/ex03/ft_calculator.py
@@ -0,0 +1,16 @@
+class calculator:
+    # your code here
+    def __add__(self, object) -> None:
+        # your code here
+        pass
+
+    def __mul__(self, object) -> None:
+        pass
+
+    def __sub__(self, object) -> None:
+        # your code here
+        pass
+
+    def __truediv__(self, object) -> None:
+        # your code here
+        pass
---
+++ piscine3/ex04/ft_calculator.py
@@ -0,0 +1,16 @@
+class calculator:
+    # your code here
+    # decorator
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
+        # your code here
+        pass
+    # decorator
+
+    def add_vec(V1: list[float], V2: list[float]) -> None:
+        # your code here
+        pass
+    # decorator
+
+    def sous_vec(V1: list[float], V2: list[float]) -> None:
+        # your code here
+        pass
