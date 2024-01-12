bcc85426383889270245920e3ca6891aedae6d14
Hyunjun KIM <hihj070914@icloud.com>
2024-01-12 21:21:15 +0900

    feat: day03 ex03

--- piscine3/ex03/ft_calculator.py
+++ piscine3/ex03/ft_calculator.py
@@ -1,16 +1,52 @@
 class calculator:
-    # your code here
-    def __add__(self, object) -> None:
-        # your code here
-        pass
+    """
+    calculator docstring
+    """
 
-    def __mul__(self, object) -> None:
-        pass
+    def __init__(self, vector: list[int | float]):
+        """
+        constructor docstring
+        """
+        self.vector = vector
+        self.len = len(vector)
 
-    def __sub__(self, object) -> None:
-        # your code here
-        pass
+    def __repr__(self):
+        """
+        representation for vector
+        """
+        return f'{self.vector}'
 
-    def __truediv__(self, object) -> None:
-        # your code here
-        pass
+    def __add__(self, object: int | float) -> None:
+        """
+        add operator docstring
+        """
+        for i in range(self.len):
+            self.vector[i] += object
+        print(self.vector)
+
+    def __mul__(self, object: int | float) -> None:
+        """
+        mul operator docstring
+        """
+        for i in range(self.len):
+            self.vector[i] *= object
+        print(self.vector)
+
+    def __sub__(self, object: int | float) -> None:
+        """
+        sub operator docstring
+        """
+        for i in range(self.len):
+            self.vector[i] -= object
+        print(self.vector)
+
+    def __truediv__(self, object: int | float) -> None:
+        """
+        truediv operator docstring
+        """
+        try:
+            for i in range(self.len):
+                self.vector[i] /= object
+            print(self.vector)
+        except ZeroDivisionError:
+            print('ZeroDivision: operand is zero')
